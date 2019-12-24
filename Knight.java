52078c66ab7e92875d664ebe50fa5a4614f91d59
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 15:37:39 -0500

    Fixed Constructor problems
    Was missing name parameter

--- Knight.java
+++ Knight.java
@@ -8,7 +8,7 @@ public class Knight extends Combatant {
   
   //Constructor
   public Knight(boolean team) {
-    super(500, 90, 1, 35, 0, team, 10, 3); //Presets stats for Knight
+    super("Knight", 500, 90, 1, 35, 0, team, 10, 3); //Presets stats for Knight
     //Knights have 500HP, 90ATK, 1 RNG, 35 DEF, 10 SPD, 3 TILE MVMT
   }
 }
