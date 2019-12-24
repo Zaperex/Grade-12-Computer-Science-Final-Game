f5f065b8a89996015d5cb108985842b9e10fbd76
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:38:11 -0500

    Moved all fields to Parent
    Sorry Sean, I changed all the other classes too to accommodate this change. This just makes damage calculations easier.

--- Combatant.java
+++ Combatant.java
@@ -6,23 +6,29 @@ import java.util.*;
 
 public class Combatant{
   //Fields
-  private String name;
-  private double health;
-  private double attack;
-  private double range;
-  private boolean team;
-  private double speed;
-  private int[] coords = new int[2];
-  private int moveDistance;
+  private String name; //Name of the combatant
+  private double health; //Health
+  private double attack; //Attack
+  private double range; //Attack Range
+  private boolean team; //Team of combatant
+  private double speed; //Speed of combatant
+  private int[] coords = new int[2]; //Coordinates of combatant
+  private int moveDistance; //How many tiles combatant can move
+  private double defense; //Defense of combatant (Reduces damage taken)
+  private double penetration; //Penetration damage of combatant (Ignore defense)
+  
   //Constructor
-  public Combatant(String name, double health, double attack, double range, boolean team, double speed, int moveDistance){
+  public Combatant(String name, double health, double attack, double range, double defense, double penetration,
+                   boolean team, double speed, int moveDistance){
     this.name = name;
-  this.health = health;
-  this.attack = attack;
-  this.range = range;
-  this.team = team;
-  this.speed = speed;
-  this.moveDistance = moveDistance;
+    this.health = health;
+    this.attack = attack;
+    this.range = range;
+    this.defense = defense;
+    this.penetration = penetration;
+    this.team = team;
+    this.speed = speed;
+    this.moveDistance = moveDistance;
   }
   
   //Setters
@@ -61,36 +67,42 @@ public class Combatant{
   public boolean getTeam(){
     return team;
   }
+  public double getDefense(){
+    return defense;
+  }
+  public double getPenetration(){
+    return penetration;
+  }
   
   //Implement when board is added
   /*
-  //Helper Methods
-  public boolean move(int x, int y){
-    int distance = Math.abs(x - coords[0]) + Math.abs(y - coords[1]);
-    if (distance <= moveDistance){
-      actualMove(x, y);
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-  
-  public void actualMove(int x, int y){
-    Scanner input = new Scanner(System.in);
-    System.out.println("Are you sure you want to move here? Y/N");
-    String response = input.nextLine();
-    while !(response.toIgnoreCase().equals("Y") || response.toIgnoreCase().equals("N")){
-      System.out.println("Are you sure you want to move here? Y/N");
-      response = input.nextLine();
-    }
-    //If confirmed then move to spot
-    if (response.toIgnoreCase().equals("Y")){
-      coords[0] = x;
-      coords[1] = y;
-    }
-    
-  }
-  */
+   //Helper Methods
+   public boolean move(int x, int y){
+   int distance = Math.abs(x - coords[0]) + Math.abs(y - coords[1]);
+   if (distance <= moveDistance){
+   actualMove(x, y);
+   return true;
+   }
+   else{
+   return false;
+   }
+   }
+   
+   public void actualMove(int x, int y){
+   Scanner input = new Scanner(System.in);
+   System.out.println("Are you sure you want to move here? Y/N");
+   String response = input.nextLine();
+   while !(response.toIgnoreCase().equals("Y") || response.toIgnoreCase().equals("N")){
+   System.out.println("Are you sure you want to move here? Y/N");
+   response = input.nextLine();
+   }
+   //If confirmed then move to spot
+   if (response.toIgnoreCase().equals("Y")){
+   coords[0] = x;
+   coords[1] = y;
+   }
+   
+   }
+   */
 }
 
