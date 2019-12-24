aea4967f633928d2044dcc01524905b4fbf2a332
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 15:38:42 -0500

    Fixed Constructor Problems
    Extends Wrong Class (misspelled parent class)
    Was missing name parameter in constructor

--- Calvary.java
+++ Calvary.java
@@ -3,10 +3,10 @@
  * Description: Subclass for the calvary troop (extends from the combatant class)
  */
 
-public class Calvary extends Combantant {
+public class Calvary extends Combatant{
   //Constructor
   public Calvary (boolean team) {
-    super(600, 100, 5, 50, 10, team, 50, 6);
+    super("Calvary", 600, 100, 5, 50, 10, team, 50, 6);
     //Calvary has 600HP, 100 ATK, 5 RNG, 50 DEF, 10 PENETRATE, 50 SPD, 6 TILE MVMT
   }
 }
