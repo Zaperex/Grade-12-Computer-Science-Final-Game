bcc668470e8fbb925b17d17dbd692b3bed987d51
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:29:31 -0500

    Updated after I moved variables to parent

--- Calvary.java
+++ Calvary.java
@@ -1,34 +1,12 @@
 /* Name : Sean Hua
  * Date: December 17, 2019
- * Description: Subclass for the calvary troop (exteds from the combatant class)
+ * Description: Subclass for the calvary troop (extends from the combatant class)
  */
 
 public class Calvary extends Combantant {
-  //Fields
-  private boolean charge;
-  private double armour;
-  
   //Constructor
-  public Calvary (boolean team, boolean charge, double armour) {
-    super(600, 100, 5, team, 50, 6);
-    this.charge = true;
-    this.armour = 50;
-  }
-  
-  //Get methods
-  public boolean getCharge () {
-    return this.charge;
-  }
-  public double getArmour () {
-    return this.armour;
-  }
-  
-  //Set methods
-  public void setCharge (boolean charge) {
-    this.charge = charge;
-  }
-  public void setArmour (double armour) {
-    this.armour = armour;
+  public Calvary (boolean team) {
+    super(600, 100, 5, 50, 10, team, 50, 6);
   }
 }
   
