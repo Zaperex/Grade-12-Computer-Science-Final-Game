87b3ccc94171620a01684909e48bd886d117606b
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:31:38 -0500

    Update Footman.java

--- Footman.java
+++ Footman.java
@@ -4,7 +4,8 @@
 class Footman extends Combatant{
   
   public Footman(boolean team){
-    super("Footman", 400, 60, 5, team, 40, 3);
+    super("Footman", 400, 60, 5, 0, 0, team, 40, 3);'
+    //Footmen have 400 HP, 60ATK, 5 RNG, 40 SPD, 3 TILE MVMT
   }
   
 }
