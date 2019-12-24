e9185b87b115688a800db6a7684481417e2fdaba
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:30:43 -0500

    Update CrossbowMen.java

--- CrossbowMen.java
+++ CrossbowMen.java
@@ -1,22 +1,10 @@
 /* Name: Frank Kong
  * Date: December 18, 2019
- * Description: CrossbowMen class that includes a penetration damage that ignores armour or defenses.
+ * Description: CrossbowMen class
  */
 
-
 public class CrossbowMen extends Combatant{
-  private double penetration;
-  private double projectileSpeed;
   public CrossbowMen(boolean team){
-    super("CrossbowMen", 500, 75, 60, team, 10, 4);
-    projectileSpeed = 300;
-  }
-  
-  public void setPenetration(double penetration){
-    this.penetration = penetration;
-  }
-  
-  public double getPenetration(){
-    return penetration;
+    super("CrossbowMen", 500, 75, 60, 5, 25, team, 10, 4);
   }
 }
