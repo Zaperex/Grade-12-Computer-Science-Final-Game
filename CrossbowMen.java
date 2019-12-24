3782f9b112caac510ce3f50206a4993d7f30dde3
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:32:51 -0500

    Update CrossbowMen.java

--- CrossbowMen.java
+++ CrossbowMen.java
@@ -6,5 +6,6 @@
 public class CrossbowMen extends Combatant{
   public CrossbowMen(boolean team){
     super("CrossbowMen", 500, 75, 60, 5, 25, team, 10, 4);
+    //Crossbowmen have 500HP, 75DMG, 60 RNG, 5 DEF, 25 PENTRATE, 10 SPD, 4 TILE MVMT
   }
 }
