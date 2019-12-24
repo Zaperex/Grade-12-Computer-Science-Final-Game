ffa1465da7ab687908816c05ff17165d4dffc5d8
Frank <50030060+Zaperex@users.noreply.github.com>
2019-12-24 14:34:01 -0500

    Update Calvary.java

--- Calvary.java
+++ Calvary.java
@@ -7,6 +7,7 @@ public class Calvary extends Combantant {
   //Constructor
   public Calvary (boolean team) {
     super(600, 100, 5, 50, 10, team, 50, 6);
+    //Calvary has 600HP, 100 ATK, 5 RNG, 50 DEF, 10 PENETRATE, 50 SPD, 6 TILE MVMT
   }
 }
   
