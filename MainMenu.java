05d2d17b4e5b47ef063aa76c31cca2bccb4dba34
HuaSean <58992881+HuaSean@users.noreply.github.com>
2020-01-01 19:26:57 -0500

    added new buttons and created a 7x7 board

--- MainMenu.java
+++ MainMenu.java
@@ -13,6 +13,8 @@ import javafx.animation.TranslateTransition;
 import javafx.animation.ScaleTransition;
 import javafx.util.Duration;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainMenu extends Application {
   
@@ -44,34 +46,64 @@ public class MainMenu extends Application {
     rect2T.setCycleCount(1);
     rect2T.play();
     
-    //Creatre a button 
-    Button startButton = new Button("Start Game");
-    //Sets the initial position of the button
-    startButton.setLayoutX(-50);
-    startButton.setLayoutY(300);
-    //Give the startButton a function (when it is clicked, it will call the startButtonClicked method)
-    startButton.setOnAction(e -> startButtonClicked(primaryStage));
+    //Create a button that will be used to start a new game
+    Button newGameButton = new Button("New Game");
+    //Sets the position of the button
+    newGameButton.setLayoutX(150);
+    newGameButton.setLayoutY(300);
+    //Give the button a function when it is pressed
+    newGameButton.setOnAction(e -> newGameButtonClicked(primaryStage));
     
-    //Transition  that will move the button to the right
-    TranslateTransition moveButton = new TranslateTransition(Duration.seconds(2), startButton);
-    //Sets where the button will move to
-    moveButton.setToX(300);
-    moveButton.setToY(0);
-    //The transition will repeat once
-    moveButton.setCycleCount(1);
-    moveButton.play();
+    //Create a button that will load the previous game
+    Button loadGameButton = new Button("Load Game");
+    //Sets the position of the button
+    loadGameButton.setLayoutX(350);
+    loadGameButton.setLayoutY(300);
+    //Give the button a function when it is pressed
+    loadGameButton.setOnAction(e -> loadGameButtonClicked());
     
     Pane root = new Pane();
     
     Scene mainMenu = new Scene(root, 600, 600);
-    root.getChildren().addAll(rect1, rect2, startButton);
-    primaryStage.setTitle("hello");
+    root.getChildren().addAll(rect1, rect2, newGameButton, loadGameButton);
+    primaryStage.setTitle("Game");
     primaryStage.setScene(mainMenu);
     primaryStage.show();
   }
-  //Method that wil change the scene and will bring the player to the game window
-  public void startButtonClicked (Stage primaryStage) {
-    //Doesn't do anything yet
+  //Method that will start a new game
+  public void newGameButtonClicked (Stage primaryStage) {
+    
+    Pane root = new Pane();
+    Scene scene2 = new Scene(root, 675, 975);
+    Button[][] button = new Button[7][7];
+    
+    for (int i = 0; i < 7; i++) {
+      for (int j = 0; j < 7; j++) {
+        button[i][j] = new Button("");
+        button[i][j].setPrefSize(75, 75);
+        button[i][j].setLayoutX(i*75 + 75);
+        button[i][j].setLayoutY(j*75 + 75);
+        
+        if (i == 0 && j == 0) {
+          button[i][j].setOnAction(e -> {
+            System.out.println("enemy base");
+          });
+        }
+        else if (i == 6 && j == 6) {
+          button[i][j].setOnAction(e -> {
+            System.out.println("home base");});
+        }
+        root.getChildren().add(button[i][j]);
+      }
+    }
+    
+    primaryStage.setScene(scene2);
+    primaryStage.show();
+    
+  }
+  //Method that will load the previous game
+  public void loadGameButtonClicked () {
+    
   }
   
   public static void main(String[] args) {
