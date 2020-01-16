88c24522ec5204c214028a92382ec919fcd6917a
Frank <50030060+Zaperex@users.noreply.github.com>
2020-01-15 23:01:19 -0500

    Update mainMethods.java

--- mainMethods.java
+++ mainMethods.java
@@ -34,26 +34,26 @@ public class mainMethods{
           p2Buildings.add(terrain[i][j].getBuilding()); 
           ((Castle)terrain[i][j].getBuilding()).setGold(150); //Initializes amount of gold player starts with
         }
-        if (x == 6 && y == 6){
+        else if (x == 6 && y == 6){
           //Stores P1 Castle in Terrain
           terrain[i][j].setBuilding(new Castle("P1", coords));
           //Stores P2 Castle in P2 Building ArrayList
           p1Buildings.add(terrain[i][j].getBuilding()); 
           ((Castle)terrain[i][j].getBuilding()).setGold(150); //Initializes amount of gold player starts with
         }
-        if (x == 3 && y == 3){
+        else if (x == 3 && y == 3){
           //Stores goldmine in terrain
           terrain[i][j].setBuilding(new GoldMine("None", coords));
           //Stores gold mine in unclaimed buildings arraylist
           unclaimedBuildings.add(terrain[i][j].getBuilding());
         }
-        if (x == 0 && y == 4){
+        else if (x == 2 && y == 6){
           //Stores goldmine in terrain
           terrain[i][j].setBuilding(new GoldMine("None", coords));
           //Stores gold mine in unclaimed buildings arraylist
           unclaimedBuildings.add(terrain[i][j].getBuilding());
         }
-        if (x == 4 && y == 0){
+        else if (x == 4 && y == 0){
           //Stores goldmine in terrain
           terrain[i][j].setBuilding(new GoldMine("None", coords));
           //Stores gold mine in unclaimed buildings arraylist
@@ -92,32 +92,37 @@ public class mainMethods{
   
   public static void loadGame(String saveFolderPath, Terrain[][] terrain, ArrayList<Troop> p1Troops, ArrayList<Troop> p2Troops, ArrayList<Building> p1Buildings,
                               ArrayList<Building> p2Buildings, ArrayList<Building> unclaimedBuildings) throws IOException{
-    File saveFolder = new File(saveFolderPath); //Creates new file using directory path
-    if (saveFolder.isDirectory()){ //Checks if file is a directory (folder)
-      //Can also use directory.listFiles() to create a File[] array
-      String[] fileNames = saveFolder.list(); //Stores the name of all files inside directory into a 2D string array
-      if (saveFolder.length() > 0){ //There will only be 5 saved files but will still check if directory is empty
-        SaveGame.loadTroops(terrain, p1Troops, fileNames[0]); //Stores info into p1Troops array
-        SaveGame.loadTroops(terrain, p2Troops, fileNames[1]); //Stores info into p2Troops array
-        SaveGame.loadBuildings(terrain, p1Buildings, fileNames[2]); //Stores info into p1Buildings array
-        SaveGame.loadBuildings(terrain, p2Buildings, fileNames[3]); //Stores info into p2Buildings array
-        turn = SaveGame.load(terrain, unclaimedBuildings, fileNames[4]); //Stores info into unclaimedBuildings and returns who's turn it is
-        //Adds troops and buildings into their respective terrain tiles
-        fillTroopTerrain(terrain, p1Troops);
-        fillTroopTerrain(terrain, p2Troops);
-        fillBuildingTerrain(terrain, p1Buildings);
-        fillBuildingTerrain(terrain, p2Buildings);
-        fillBuildingTerrain(terrain, unclaimedBuildings);
+    try{
+      File saveFolder = new File(saveFolderPath); //Creates new file using directory path
+      if (saveFolder.isDirectory()){ //Checks if file is a directory (folder)
+        //Can also use directory.listFiles() to create a File[] array
+        String[] fileNames = saveFolder.list(); //Stores the name of all files inside directory into a 2D string array
+        if (saveFolder.length() > 0){ //There will only be 5 saved files but will still check if directory is empty
+          SaveGame.loadTroops(terrain, p1Troops, fileNames[0]); //Stores info into p1Troops array
+          SaveGame.loadTroops(terrain, p2Troops, fileNames[1]); //Stores info into p2Troops array
+          SaveGame.loadBuildings(terrain, p1Buildings, fileNames[2]); //Stores info into p1Buildings array
+          SaveGame.loadBuildings(terrain, p2Buildings, fileNames[3]); //Stores info into p2Buildings array
+          turn = SaveGame.load(terrain, unclaimedBuildings, fileNames[4]); //Stores info into unclaimedBuildings and returns who's turn it is
+          //Adds troops and buildings into their respective terrain tiles
+          fillTroopTerrain(terrain, p1Troops);
+          fillTroopTerrain(terrain, p2Troops);
+          fillBuildingTerrain(terrain, p1Buildings);
+          fillBuildingTerrain(terrain, p2Buildings);
+          fillBuildingTerrain(terrain, unclaimedBuildings);
+        }
+        else{
+          p1Troops.clear();
+          p2Troops.clear();
+          p1Buildings.clear();
+          p2Buildings.clear();
+          unclaimedBuildings.clear();
+          //If files are not there, then start a new game
+          newGame(terrain, p1Troops, p2Troops, p1Buildings, p2Buildings, unclaimedBuildings);
+        }
       }
-      else{
-        p1Troops.clear();
-        p2Troops.clear();
-        p1Buildings.clear();
-        p2Buildings.clear();
-        unclaimedBuildings.clear();
-        //If files are not there, then start a new game
-        newGame(terrain, p1Troops, p2Troops, p1Buildings, p2Buildings, unclaimedBuildings);
-      }
+    }
+    catch (IOException e){
+      System.out.println("An IOException has occured");
     }
   }
   //Method that helps recruit the troops and place them in the tile where they are recruited
@@ -134,5 +139,49 @@ public class mainMethods{
       return true;
     }
   }
+  //Method that makes troop claim an unclaimed gold mine or building
+  public static void claimBuilding(Terrain[][] terrain, ArrayList<Building> unclaimedBuildings, ArrayList<Building> playerBuildings, Troop troop){
+    //Stores coords
+    int x = troop.getCoords()[0];
+    int y = troop.getCoords()[1];
+    int[] coords = {x, y}; 
+    
+    for (int i = 0; i < unclaimedBuildings.size(); i++){
+      Building building = unclaimedBuildings.get(i);
+      if (building.getCoords().equals(coords)){
+        playerBuildings.add(building); //Adds building to player's building arraylist
+        unclaimedBuildings.remove(building); //Removes building from the unclaimed building arraylist
+      }
+      break; //There will only be one building needed to be found so break after finding it
+    }
+  }
+    
+  //Method that changes the turn and updates everything for the turn of the player
+  public static String turnFlip(String turn, Terrain[][] terrain, ArrayList<Troop> p1Troops, ArrayList<Troop> p2Troops, ArrayList<Building> p1Buildings,
+                              ArrayList<Building> p2Buildings, ArrayList<Building> unclaimedBuildings){
+    String newTurn;
+    //If it's currently P1, 
+    if (turn.equals("P1")){
+      newTurn = "P2";
+      Castle castle = terrain[6][6].getBuilding();
+      for (int i = 0; i < p1Troops.size(); i++){
+        p1Troop.get(i).setAction(true); //Sets the action of each troop to true
+      }
+      //Adds gold income equal to the number of buildings times 100
+      castle.setGold(castle.getGold() + p2Buildings.size()*100);
+    }
+    else {
+      newTurn = "P1";
+      Castle castle = terrain[0][0].getBuilding();
+      for (int i = 0; i < p2Troops.size(); i++){
+        p2Troop.get(i).setAction(true); //Sets the action of each troop to true
+      }
+      //Adds gold income equal to the number of buildings times 100
+      castle.setGold(castle.getGold() + p1Buildings.size()*100);
+    }
+      
+      return turn;
+  }
+    
 }
 
